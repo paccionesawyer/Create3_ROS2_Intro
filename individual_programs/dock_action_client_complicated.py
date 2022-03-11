10da77235d0b95dafd9a6f63093375faa1c31511
paccionesawyer <sawyer.b.paccione@gmail.com>
2022-03-10 23:50:32 -0500

    Created Future Examples

---
+++ individual_programs/dock_action_client_complicated.py
@@ -0,0 +1,54 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+from irobot_create_msgs.action import DockServo
+
+
+class DockServoActionClient(Node):
+
+    def __init__(self):
+        super().__init__('dockservo_action_client')
+        self._action_client = ActionClient(self, DockServo, 'dock')
+
+    def send_goal(self, order):
+        goal_msg = DockServo.Goal()
+
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+        return self._send_goal_future
+
+    def goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result))
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_client = DockServoActionClient()
+    future = action_client.send_goal("{}")
+    rclpy.spin_until_future_complete(action_client, future)
+
+    # action_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ individual_programs/undock_action_client_complicated.py
@@ -0,0 +1,58 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+from irobot_create_msgs.action import Undock
+
+# TODO Simplify this code (i.e. less callback functions and no future)
+
+class UndockingActionClient(Node):
+
+    def __init__(self):
+        super().__init__('undocking_action_client')
+        self._action_client = ActionClient(self, Undock, 'undock')
+
+    def send_goal(self, order):
+        goal_msg = Undock.Goal()
+
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+        return self._send_goal_future
+
+    def goal_response_callback(self, future):
+        print("here")
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result))
+
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_client = UndockingActionClient()
+    future = action_client.send_goal("{}")
+    rclpy.spin_until_future_complete(action_client, future)
+
+    # action_client.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
