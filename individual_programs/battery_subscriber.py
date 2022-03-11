341ef7915da65cc44734743a74a91f47868fa261
paccionesawyer <sawyer.b.paccione@gmail.com>
2022-03-10 23:49:58 -0500

    Added Namespace variable

--- individual_programs/battery_subscriber.py
+++ individual_programs/battery_subscriber.py
@@ -1,25 +1,43 @@
-import sys,rclpy
+import sys
+import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from sensor_msgs.msg import BatteryState
 
-class BatterySubscriber(Node):
 
-    def __init__(self):
+class BatterySubscriber(Node):
+    '''
+    A Node listening to the /battery_state topic.
+    '''
+
+    def __init__(self, namespace: str = ""):
         super().__init__('battery_subscriber')
         self.subscription = self.create_subscription(
-            BatteryState,'/battery_state',self.listener_callback,qos_profile_sensor_data)        
-        self.subscription  # prevent unused variable warning
+            BatteryState, namespace + '/battery_state', self.listener_callback,
+            qos_profile_sensor_data)
 
-    def listener_callback(self, msg):
+    def listener_callback(self, msg: BatteryState):
+        """
+        :type msg: BatteryState
+        :rtype: None
+        """
         self.get_logger().info('I heard: "%s"' % msg)
+        self.printBattery(msg)
+
+    def printBattery(self, msg):
+        """
+        :type msg: BatteryState
+        :rtype: None
+        """
+        # We can get components of the message by using the '.' dot operator
+        print("Battery Percentage:", msg.percentage)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
     battery_subscriber = BatterySubscriber()
-    try: 
+    try:
         rclpy.spin(battery_subscriber)
     except KeyboardInterrupt:
         print('Caught keyboard interrupt')
@@ -29,6 +47,7 @@ def main(args=None):
         print("done")
         battery_subscriber.destroy_node()
         rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
