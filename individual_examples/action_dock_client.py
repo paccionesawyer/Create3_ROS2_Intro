e6d6ed2e3556158d8e595fb01dfd6b4ec018638d
paccionesawyer <sawyer.b.paccione@gmail.com>
2022-03-15 18:53:24 -0400

    Update action_dock_client.py

--- individual_examples/action_dock_client.py
+++ individual_examples/action_dock_client.py
@@ -4,47 +4,45 @@ from rclpy.node import Node
 
 from irobot_create_msgs.action import DockServo
 
-
 class DockServoActionClient(Node):
+    '''
+    A simple example of an action client that will cause the iRobot Create3
+    to dock if it is currently undocked. Subclass of Node.
+    '''
 
     def __init__(self):
+        '''
+        Purpose
+        -------
+        initialized by calling the Node constructor, naming our node 
+        'dockservo_action_client'
+        '''
         super().__init__('dockservo_action_client')
         self._action_client = ActionClient(self, DockServo, 'dock')
 
     def send_goal(self):
+        '''
+        Purpose
+        -------
+        This method waits for the action server to be available, then sends a 
+        goal to the server. It returns a future that we can later wait on.
+        '''
         goal_msg = DockServo.Goal()
         print(goal_msg)
 
         self._action_client.wait_for_server()
-
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
-
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
-
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :( Check to see if your Create3 is currently docked')
-            return
-
-        self.get_logger().info('Goal accepted :)')
-
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result))
-        rclpy.shutdown()
+        return self._action_client.send_goal_async(goal_msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    action_client = DockServoActionClient()
-    action_client.send_goal()
-    rclpy.spin(action_client)
+    dock_client = DockServoActionClient()
+    future = dock_client.send_goal()
 
+    # The future is completed when an action server accepts or rejects the goal.
+    rclpy.spin_until_future_complete(dock_client, future)
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
