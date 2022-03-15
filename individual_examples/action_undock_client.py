f55ca04cd1b6d901755b7c2f690c30ce53919a53
paccionesawyer <sawyer.b.paccione@gmail.com>
2022-03-15 18:54:54 -0400

    Less complicated

--- individual_examples/action_undock_client.py
+++ individual_examples/action_undock_client.py
@@ -7,51 +7,44 @@ from irobot_create_msgs.action import Undock
 
 class UndockingActionClient(Node):
     '''
-    Purpose
-    -------
     A simple action client to communicate to the /undock action, this will
     only work if the Create3 is currently docked.
     '''
 
     def __init__(self):
+        '''
+        Purpose
+        -------
+        initialized by calling the Node constructor, naming our node 
+        'undocking_action_client'
+        '''
         super().__init__('undocking_action_client')
         self._action_client = ActionClient(self, Undock, 'undock')
 
     def send_goal(self):
+        '''
+        Purpose
+        -------
+        This method waits for the action server to be available, then sends a 
+        goal to the server. It returns a future that we can later wait on.
+        '''
         goal_msg = Undock.Goal()
         print(goal_msg)
 
         self._action_client.wait_for_server()
 
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
-
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
-
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :( Check that the Create3 is currently docked')
-            return
-
-        self.get_logger().info('Goal accepted :)')
-
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
-
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result))
-        rclpy.shutdown()
+        return self._action_client.send_goal_async(goal_msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    action_client = UndockingActionClient()
+    undock_client = UndockingActionClient()
+    future = undock_client.send_goal()
 
-    action_client.send_goal()
-
-    rclpy.spin(action_client)
+    # The future is completed when an action server accepts or rejects the goal.
+    rclpy.spin_until_future_complete(undock_client, future)
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
