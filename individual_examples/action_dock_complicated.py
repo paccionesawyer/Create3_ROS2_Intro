1a6b7f54697ee80903d187d8331a2257a4cab3af
paccionesawyer <sawyer.b.paccione@gmail.com>
2022-03-15 18:53:35 -0400

    Create action_dock_complicated.py

---
+++ individual_examples/action_dock_complicated.py
@@ -0,0 +1,79 @@
+import rclpy
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+from irobot_create_msgs.action import DockServo
+
+
+class DockServoActionClient2(Node):
+    '''
+    A more complicated example of an action client that will cause the iRobot 
+    Create3 to dock if it is currently undocked. Subclass of Node.
+    '''
+
+    def __init__(self):
+        '''
+        Purpose
+        -------
+        initialized by calling the Node constructor, naming our node 
+        'dockservo_action_client'
+        '''
+        super().__init__('dockservo_action_client')
+        self._action_client = ActionClient(self, DockServo, 'dock')
+
+    def send_goal(self, order):
+        '''
+        Purpose
+        -------
+        This method waits for the action server to be available, then sends a 
+        goal to the server. It returns a future that we can later wait on.
+        '''
+        goal_msg = DockServo.Goal()
+
+        self._action_client.wait_for_server()
+        self._send_goal_future = self._action_client.send_goal_async(
+            goal_msg)
+        self._send_goal_future.add_done_callback(self.goal_response_callback)
+
+        return self._send_goal_future
+
+    def goal_response_callback(self, future):
+        '''
+        Purpose
+        -------
+        A callback that is executed when the future is complete.
+        The future is completed when an action server accepts or rejects the goal request.
+        '''
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            # Return early so there is no result
+            self.get_logger().info('Goal rejected :(')
+            return
+
+        self.get_logger().info('Goal accepted :)')
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.get_result_callback)
+
+    def get_result_callback(self, future):
+        '''
+        Purpose
+        -------
+        Similar to sending the goal, we will get a future that will complete when the result is ready.
+        '''
+        result = future.result().result
+        self.get_logger().info('Result: {0}'.format(result))
+        rclpy.shutdown()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    action_client = DockServoActionClient2()
+    action_client.send_goal("{}")
+    rclpy.spin(action_client)
+
+    # action_client.destroy_node()
+
+
+if __name__ == '__main__':
+    main()
