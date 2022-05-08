292ae29acf0b1627214bdafaf746ff822ae92ba6
paccionesawyer <sawyer.b.paccione@gmail.com>
2022-05-08 14:45:43 -0400

    Create custom_ROS2_progarm.py

---
+++ custom_ROS2_progarm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+class ROS2_program_creator():
+    def __init__(self) -> None:
+        pass
+
+    def get_sub_topics() -> list:
+        '''
+        Prompt the user and return the list of topics you want to subscribe to
+        '''
+
+    def get_pub_topics() -> list:
+
+    def create_file() -> StringIO:
+        with open('readme.txt', 'w') as f:
+            f.write('Create a new text file!')
